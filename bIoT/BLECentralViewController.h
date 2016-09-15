e599a3fd180c9029ce308ed7b74c6e06db1240d3
root <root@berry-love.tk>
2016-09-15 17:31:42 +0900

    first commit

---
+++ bIoT/BLECentralViewController.h
@@ -0,0 +1,12 @@
+//
+//  bCentral
+//
+//  Created by Yuichiro Nakada on 2016/09/09.
+//  Copyright © 2016 Yuichiro Nakada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLECentralViewController : UIViewController
+
+@end
---
+++ bIoT/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  bCentral
+//
+//  Created by Yuichiro Nakada on 2016/09/09.
+//  Copyright © 2016 Yuichiro Nakada. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+	[super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+	[super didReceiveMemoryWarning];
+}
+
+@end
